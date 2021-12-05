04901c80f3ae6ba7b16fc4f10ecf631e82e16baa
Tim <timsalguz@gmail.com>
2021-12-05 14:13:44 +0200

    + Settings

--- main.cpp
+++ main.cpp
@@ -7,7 +7,13 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
-#define MAXELEMENTSONSCREEN 1000
+#define MAXELEMENTSONSCREEN 10000
+#define POSKOLKOSOZDAVAT 1
+#define MAXRECTWIDTH 5
+#define MAXRECTHEIGHT 100
+#define VYSOTAPOYAVLENIYA -1000
+#define VYSOTAISCHEZANIYA 0
+
 const int windowWidth = 1366;
 const int windowHeight = 768;
 
@@ -32,11 +38,13 @@ int main()
                 window.close();
         }
         if(rectangles.size() < MAXELEMENTSONSCREEN)
-        rectangles.push_back(new ts::Rectangle ({ (double (rand()%windowWidth)), -150 }, 1+(rand()%20), 1+(rand()%20), windowHeight/(double (rand()%1000))));
 
+            for (int i = 0; i < POSKOLKOSOZDAVAT; ++i) {
+                rectangles.push_back(new ts::Rectangle ({ (double (rand()%windowWidth)), VYSOTAPOYAVLENIYA }, 1+(rand()%MAXRECTWIDTH), 1+(rand()%MAXRECTHEIGHT), windowHeight/(double (rand()%1000))));
+            }
 
         for(int i = 0; i < rectangles.size(); i++)
-            if ((rectangles[i]->GetY()) <= windowHeight - 10)
+            if ((rectangles[i]->GetY()) <= windowHeight - VYSOTAISCHEZANIYA)
                 rectangles[i]->Move();
             else
                 rectangles.erase(rectangles.begin() + i);
